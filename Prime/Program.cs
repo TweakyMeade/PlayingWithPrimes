f0707ad7e06f05543e3e74ca21afe1077df5cecb
Anthony Meade <antnymeade@gmail.com>
2024-10-01 21:00:01 +0100

    Update Program.cs

--- Prime/Program.cs
+++ Prime/Program.cs
@@ -8,7 +8,7 @@ namespace Prime
         {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            string results = String.Join('\n',PrimeMethods.processPrimes(1));
+            string results = String.Join('\n',PrimeMethods.processPrimes(100000));
             stopWatch.Stop();
             File.WriteAllText("Results.txt",$"Time in Milliseconds:{stopWatch.ElapsedMilliseconds.ToString()}\n\n{results}");
         }
